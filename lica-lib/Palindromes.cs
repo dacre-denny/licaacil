ded8b1a09e541fd61b6fd3eb2dd5b569377098ea
dacre <dacre@mooce.co.nz>
2016-12-18 10:27:00 +1300

    - first implementation of isPalindromeInAnagramOf(..) method

--- lica-lib/Palindromes.cs
+++ lica-lib/Palindromes.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,39 @@ namespace lica_lib
 {
     public class Palindromes
     {
+
         public static bool isPalindromeInAnagramOf(string inputString)
         {
-            throw new NotImplementedException();
+            Func<int, bool> isCountEven = (count => count % 2 == 0);
+            Func<int, bool> isCountOdd = (count => count % 2 == 1);
+
+            Dictionary<char, int> charCounts = new Dictionary<char, int>();
+            
+            foreach(char c in inputString)
+            {
+                if(charCounts.ContainsKey(c))
+                {
+                    charCounts[c]++;
+                }
+                else
+                {
+                    charCounts.Add(c, 1);
+                }
+            }
+            
+            var charOccurences = charCounts.Values;
+
+            if (charOccurences.All(isCountEven))
+            {
+                return true;
+            }
+
+            if(charOccurences.Count(isCountOdd) == 1)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
