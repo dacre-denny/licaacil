8c4a7ebf8a5f3e7b261ceb98ef9e11aa26a27b2e
dacre <dacre@mooce.co.nz>
2016-12-18 09:10:21 +1300

    - define method stub first, allowing test driven approach to development

---
+++ lica-lib/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lica_lib
+{
+    public class Helpers
+    {
+        public static bool isPalindromeInAnagramOf(string inputString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
